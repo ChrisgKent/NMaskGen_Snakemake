da6563d11ed5a3a45057afd9cedfba079072b620
ChrisgKent <chrisgkent@gmail.com>
2022-01-15 12:05:01 +0000

    Added the mask_gen for snakemake

--- workflow/scripts/mask_gen.py
+++ workflow/scripts/mask_gen.py
@@ -1,19 +1,38 @@
 from Bio import SeqIO
-from Bio.Align.Applications import ClustalOmegaCommandline
-from Bio.Seq import Seq
-from Bio.Seq import MutableSeq
 from Bio.SeqRecord import SeqRecord
+from Bio.Seq import MutableSeq
+import csv
 
 
-def mask_gen(list_of_pos, ref):
+def main(
+    bed_dir=snakemake.input["bed_dir"],
+    ref_dir=snakemake.input["ref_dir"],
+    output_dir=snakemake.output[0],
+):
     """
     Given a list of positions and a referance sequence. This will mask all positions with an N.
     The mutated sequence is returned as a Seq object
     """
-    Mut_seq = MutableSeq(ref)
+    # Read in the combined bed_file
+    with open(bed_dir) as file:
+        tsv_file = csv.reader(file, delimiter="\t")
+        positions = [x[1] for x in tsv_file]
 
-    for i in range(len(Mut_seq)):
-        if i in list_of_pos:
-            Mut_seq[i] = "N"
+    # Reads in the referance genomes and assigns the name and seq
+    ref_it = SeqIO.parse(ref_dir, "fasta")
+    ref_seqrecord = [x for x in ref_it]
 
-    return Mut_seq
+    mut_seq = MutableSeq(ref_seqrecord[0].seq)
+
+    for i in positions:
+        mut_seq[int(i)] = "N"
+
+    SeqIO.write(
+        SeqRecord(mut_seq, id="NMask", description=""),
+        output_dir,
+        "fasta",
+    )
+
+
+if __name__ == "__main__":
+    main()
